d318d95e3f691d564060133e7c006b3b81d2c1b2
BasharKhan <basharkhan71@gmail.com>
2026-06-26 11:12:53 +0600

    search in rotated array

--- src/binary_search/SearchInRotatedSortedArray.java
+++ src/binary_search/SearchInRotatedSortedArray.java
@@ -12,10 +12,26 @@ public class SearchInRotatedSortedArray {
                 return mid;
             }
 
-            if(nums[l] > nums[r] && nums[mid] > target) {
-                l = mid + 1;
+            if (nums[l] < nums[r]) {
+                if (target > nums[mid]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             } else {
-                r = mid - 1;
+                if (nums[l] <= nums[mid]) {
+                    if (target >= nums[l] && target <= nums[mid]) {
+                        r = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                } else {
+                    if (target >= nums[mid] && target <= nums[r]) {
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
             }
         }
 
@@ -25,12 +41,13 @@ public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
         int[] nums={0,1,2};
         //int[] nums={1,0};
-        //int[] nums={3,4,5,6,1,2};
+        //int[] nums={4,5,6,7,0,1,2};
+        //int[] nums={5,1,2,3,4};
         //int[] nums={4,5,6,7,0,1,2};
         //int[] nums={1,3};
-        int target = 2;
+        int target = 5;
 
         int index = search(nums, target);
-        System.out.println(index);
+        System.out.println("Index: " + index);
     }
 }
