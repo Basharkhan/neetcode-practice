b9d2eaed633254b2cbfbddfebb63c3e6adc07e07
Bashar Khan <basharkhan71@gmail.com>
2026-08-16 17:52:04 +0600

    k frequent elements

--- src/top_k_frequent_elements/Solution2.java
+++ src/top_k_frequent_elements/Solution2.java
@@ -8,13 +8,30 @@ import java.util.PriorityQueue;
 public class Solution2 {
     public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> frequency = new HashMap<>();
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int num: nums) {
             frequency.put(num, frequency.getOrDefault(num, 0) + 1);
         }
 
-        return null;
+        PriorityQueue<Integer> queue =
+                new PriorityQueue<>((a, b) -> frequency.get(a) - frequency.get(b));
+
+        for (int num: frequency.keySet()) {
+            queue.offer(num);
+
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+
+        // producing result
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            result[i] = queue.poll();
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
