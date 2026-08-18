06651948108d5ad351ed957dcc7f292285bea655
Bashar Khan <basharkhan71@gmail.com>
2026-08-18 17:28:08 +0600

    encode and decode

--- src/encode_and_decode_string/Solution2.java
+++ src/encode_and_decode_string/Solution2.java
@@ -1,5 +1,6 @@
 package encode_and_decode_string;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution2 {
@@ -15,20 +16,29 @@ public class Solution2 {
     }
 
     public static List<String> decode(String str) {
-        System.out.println(str);
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '#' && Character.isDigit(str.charAt(i - 1))) {
-                System.out.println(i);
-            }
+        System.out.println("Encoded: " + str);
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < str.length()) {
+            int delimiter = str.indexOf('#', i);
+            int length = Integer.parseInt(str.substring(i, delimiter));
+            int start = delimiter + 1;
+            int end = start + length;
+            String word = str.substring(start, end);
+            result.add(word);
+            i = end;
         }
 
-        return null;
+        return result;
     }
 
     public static void main(String[] args) {
-        List<String> original = List.of("neet", "code", "love", "you");
+        List<String> original = List.of("neet", "codeeeeeeeeee", "love", "you");
 
         String encoded = encode(original);
         List<String> decoded = decode(encoded);
+        System.out.println(decoded);
     }
 }
