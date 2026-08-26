c7ed296be4fb8d296c5528e109af4b1de41e0200
BasharKhan <basharkhan71@gmail.com>
2026-08-26 11:24:20 +0600

    lcs

--- src/longest_consecutive_sequence/Solution3.java
+++ src/longest_consecutive_sequence/Solution3.java
@@ -1,37 +1,39 @@
 package longest_consecutive_sequence;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Solution3 {
     public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+
         Set<Integer> set = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
 
         for (int num: nums) {
             set.add(num);
         }
 
-        int starting = -1;
+        int longest = 0;
 
-        for (int num: nums) {
+        for (int num: set) {
             if (!set.contains(num - 1)) {
-                System.out.println("Starting: " + num);
-                if (set.contains(num + 1)) {
-                    System.out.println(num + 1);
+                int count = 1;
+                while (set.contains(num + 1)) {
+                    num++;
+                    count++;
                 }
+                longest = Math.max(longest, count);
             }
         }
-        System.out.println(list);
-        return 0;
+
+        return longest;
     }
 
     public static void main(String[] args) {
         int[] nums = {100, 4, 200, 1, 2, 3};
+        // int[] nums = {1};
         int longestConsecutive = longestConsecutive(nums);
-        System.out.println("Hi: " + longestConsecutive);
+        System.out.println("Count: " + longestConsecutive);
     }
 
 }
