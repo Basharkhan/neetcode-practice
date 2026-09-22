4ed78ac878c503c5c0cba271fe208ef92fbb4b62
Bashar Khan <basharkhan71@gmail.com>
2026-09-22 17:01:20 +0600

    threesum

--- src/three_sum/Solution2.java
+++ src/three_sum/Solution2.java
@@ -8,11 +8,35 @@ public class Solution2 {
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> lists = new ArrayList<>();
         Arrays.sort(nums);
-        int target = 0;
 
-//        while () {
-//
-//        }
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+                    lists.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+
+                    while (l < r && (nums[l] == nums[l - 1])) {
+                        l++;
+                    }
+
+                    while (l < r && (nums[r] == nums[r + 1])) {
+                        r--;
+                    }
+                } else if (sum > 0) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
 
         return lists;
     }
---
+++ src/valid_palindrome/Solution2.java
@@ -0,0 +1,11 @@
+package valid_palindrome;
+
+public class Solution2 {
+    public static boolean isPalindrome(String s) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
