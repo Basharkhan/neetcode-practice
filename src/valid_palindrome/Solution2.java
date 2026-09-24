66ec2fc4c992686dce6772f2a4cc4c8daa8af41e
Bashar Khan <basharkhan71@gmail.com>
2026-09-24 17:42:20 +0600

    palindrome

--- src/valid_palindrome/Solution2.java
+++ src/valid_palindrome/Solution2.java
@@ -2,10 +2,33 @@ package valid_palindrome;
 
 public class Solution2 {
     public static boolean isPalindrome(String s) {
-        return false;
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            }
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        String s = "A man, a plan, a canal: Panama"; // → true
+        // String s = "noon"; // true
+        // String s = "race a car"; // → false
+        // String s = " "; // → true
+        boolean palindrome = isPalindrome(s);
+        System.out.println("Result: " + palindrome);
     }
 }
